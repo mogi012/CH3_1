9bcb0b0927745ea0d0b6b92a4328f5fb6455f9f6
jinmok0317-hue <jinmok0317@gmail.com>
2026-08-17 10:05:13 +0900

    Initial commit

---
+++ Source/CH3_1/MovingBlock.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingBlock.h"
+
+// Sets default values
+AMovingBlock::AMovingBlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+}
+
+// Called when the game starts or when spawned
+void AMovingBlock::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	MoveDirection = MoveDirection.GetSafeNormal();
+	
+}
+
+// Called every frame
+void AMovingBlock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CurrentOffset += MoveSpeed * DeltaTime * MoveSign;
+
+	if (CurrentOffset >= MaxRange)
+	{
+		CurrentOffset = MaxRange;
+		MoveSign = -1.0f;
+	}
+	else if (CurrentOffset <= -MaxRange)
+	{
+		CurrentOffset = -MaxRange;
+		MoveSign = 1.0f;
+	}
+
+	SetActorLocation(StartLocation + MoveDirection * CurrentOffset);
+}
+
---
+++ Source/CH3_1/MovingBlock.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "MovingBlock.generated.h"
+
+UCLASS()
+class CH3_1_API AMovingBlock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingBlock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MoveSpeed = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MaxRange = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	FVector MoveDirection = FVector(0.0f, 0.0f, 1.0f);
+
+private:
+
+	FVector StartLocation;
+
+	float CurrentOffset = 0.0f;
+
+	float MoveSign = 1.0f;
+
+};
